591fb5a218a695c3aa3b165a5075343e0bc307e1
João Santos <jviniciussantos13@gmail.com>
2024-11-07 18:07:37 -0300

    base1

--- app/src/main/java/com/example/project4/MyDatabaseHelper.kt
+++ app/src/main/java/com/example/project4/MyDatabaseHelper.kt
@@ -6,53 +6,65 @@ import android.database.sqlite.SQLiteOpenHelper
 class MyDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         private const val DATABASE_NAME = "MyDatabase.db"
-        private const val DATABASE_VERSION = 1
-        private const val SQL_CREATE_TABLE = "CREATE TABLE MyTable (" +
+        private const val DATABASE_VERSION = 2
+        private const val SQL_CREATE_TABLE = "CREATE TABLE Diary (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "name TEXT," +
-                "age INTEGER)"
+                "titulo TEXT," +
+                "data TEXT," +
+                "pesadelo TEXT," +
+                "descricao INTEGER)"
 
     }
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(SQL_CREATE_TABLE)
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL("DROP TABLE IF EXISTS MyTable")
+        db.execSQL("DROP TABLE IF EXISTS Diary")
         onCreate(db)
     }
-    fun insertData(name: String, age: Int): Long {
+    fun insertData(titulo: String, data: String, pesadelo: String, descricao: Int): Long {
         val db = writableDatabase
         val values = ContentValues().apply {
-            put("name", name)
-            put("age", age)
+            put("titulo", titulo)
+            put("data", data)
+            put("pesadelo", pesadelo)
+            put("descricao", descricao)
         }
-        return db.insert("MyTable", null, values)
+        return db.insert("Diary", null, values)
     }
     fun readData(): List<String> {
         val db = readableDatabase
-        val projection = arrayOf("id", "name", "age")
+        val projection = arrayOf("id", "titulo", "data", "pesadelo", "descricao")
         val cursor = db.query(
-            "MyTable",
-            projection,
-            null,
-            null,
-            null,
-            null,
-            null
+            "Diary", // Nome da tabela
+            projection, // Colunas a serem recuperadas
+            null, // Sem cláusula WHERE
+            null, // Sem argumentos para o WHERE
+            null, // Sem agrupamento
+            null, // Sem filtro de grupo de linhas
+            null  // Sem ordenação específica
         )
 
         val itemList = mutableListOf<String>()
         with(cursor) {
             while (moveToNext()) {
-                val id = getInt(getColumnIndexOrThrow("id"))
-                val name = getString(getColumnIndexOrThrow("name"))
-                val age = getInt(getColumnIndexOrThrow("age"))
-                itemList.add("ID: $id, Name: $name, Age: $age")
+                try {
+                    val id = getInt(getColumnIndexOrThrow("id"))
+                    val titulo = getString(getColumnIndexOrThrow("titulo"))
+                    val data = getString(getColumnIndexOrThrow("data"))
+                    val pesadelo = getString(getColumnIndexOrThrow("pesadelo"))
+                    val descricao = getInt(getColumnIndexOrThrow("descricao"))
+
+                    itemList.add("ID: $id, Titulo: $titulo, Data: $data, Pesadelo: $pesadelo, Descricao: $descricao")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
         cursor.close()
         return itemList
     }
+
 
     fun deleteData(id: Int): Int {
         val db = writableDatabase
@@ -60,7 +72,7 @@ class MyDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NA
         val selection = "id = ?"
         val selectionArgs = arrayOf(id.toString())
         // Executa o método delete e retorna o número de linhas excluídas
-        return db.delete("MyTable", selection, selectionArgs)
+        return db.delete("Diary", selection, selectionArgs)
     }
 
 }
--- app/src/main/java/com/example/project4/SecondActivity.kt
+++ app/src/main/java/com/example/project4/SecondActivity.kt
@@ -1,39 +1,52 @@
 package com.example.project4
 import MyDatabaseHelper
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project4.R
+import java.util.Calendar
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var dbHelper: MyDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
         dbHelper = MyDatabaseHelper(this)
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val editTextAge = findViewById<EditText>(R.id.editTextAge)
+
+        val tituloText = findViewById<EditText>(R.id.tituloText)
+        val pesadeloText = findViewById<EditText>(R.id.pesadeloText)
         val buttonInsert = findViewById<Button>(R.id.buttonInsert)
         val buttonView = findViewById<Button>(R.id.buttonView)
         val textViewData = findViewById<TextView>(R.id.textViewData)
         val delBtn = findViewById<Button>(R.id.buttonDelete)
         val delText = findViewById<TextView>(R.id.delText)
+        val classText = findViewById<EditText>(R.id.classText)
+        val dataText = findViewById<EditText>(R.id.dataText)
 
         buttonInsert.setOnClickListener {
-            val name = editTextName.text.toString()
-            val age = editTextAge.text.toString().toIntOrNull()
-            if (name.isNotEmpty() && age != null) {
-                dbHelper.insertData(name, age)
-                editTextName.text.clear()
-                editTextAge.text.clear()
+            val titulo = tituloText.text.toString()
+            val pesadelo = pesadeloText.text.toString()
+            val classificação = classText.text.toString().toIntOrNull()
+            val data = dataText.text.toString()
+
+            if (titulo.isNotEmpty() && data.isNotEmpty() && pesadelo.isNotEmpty() && classificação != null) {
+                dbHelper.insertData(titulo, data, pesadelo,classificação)
+
+                tituloText.text.clear()
+                dataText.text.clear()
+                pesadeloText.text.clear()
+                classText.text.clear()
             }
         }
-        // Evento de clique para visualizar dados
+
+
         buttonView.setOnClickListener {
-            val data = dbHelper.readData()
-            textViewData.text = data.joinToString("\n")
+            val info = dbHelper.readData()
+            textViewData.text = info.joinToString("\n")
         }
 
         delBtn.setOnClickListener{
@@ -41,5 +54,17 @@ class SecondActivity : AppCompatActivity() {
             dbHelper.deleteData(id)
         }
 
+        dataText.setOnClickListener{
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                // Formata a data selecionada como "yyyy-MM-dd"
+                val formattedDate = String.format("%04d-%02d-%02d", selectedYear, selectedMonth + 1, selectedDay)
+                dataText.setText(formattedDate)
+            }, year, month, day).show()
+        }
     }
 }
